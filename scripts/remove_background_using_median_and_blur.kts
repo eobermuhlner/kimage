c84e651d0a0e5a04c77e4dd5b5d6d4a4a1931209
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-03-26 09:16:08 +0100

    add scripts folder with examples

---
+++ scripts/remove_background_using_median_and_blur.kts
@@ -0,0 +1,17 @@
+import ch.obermuhlner.kimage.*
+import ch.obermuhlner.kimage.filter.*
+import ch.obermuhlner.kimage.image.*
+import kotlin.math.min
+import kotlin.math.max
+
+println("Background removal using median filter + gaussian blur")
+println("Input file = $inputFile")
+println("Input = $input")
+
+val size = max(1, min(input.width, input.height) / 100)
+//val size = 5
+println("Kernel size = $size")
+
+val background = input.median(size * 2).gaussianBlur(size)
+input - background * 1.1
+
