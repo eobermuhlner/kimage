66cc7bab1d91efdd31a17329febbbda1dc345356
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-04-10 23:55:24 +0200

    support TiffReader

--- src/main/kotlin/ch/obermuhlner/kimage/io/ImageReader.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/io/ImageReader.kt
@@ -3,12 +3,24 @@ package ch.obermuhlner.kimage.io
 import ch.obermuhlner.kimage.image.Image
 import ch.obermuhlner.kimage.image.MatrixImage
 import ch.obermuhlner.kimage.image.awt.AwtBufferedImage
+import ch.obermuhlner.kimage.image.tiff.TiffImage
+import mil.nga.tiff.TiffReader
 import java.io.File
 import javax.imageio.ImageIO
 
 object ImageReader {
 
     fun read(file: File): Image {
+        if (file.extension.toLowerCase() in listOf("tif", "tiff")) {
+            try {
+                val tiffImage = TiffReader.readTiff(file)
+                return TiffImage(tiffImage, false)
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                // ignore
+            }
+        }
+
         val image = ImageIO.read(file) ?: throw RuntimeException("Failed to read image: $file")
         return AwtBufferedImage(image)
     }
