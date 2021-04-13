d1aace25bda6a2e665c2340ba14128affbaa6453
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-04-13 18:03:24 +0200

    use averageError() with default channels

--- src/main/kotlin/ch/obermuhlner/kimage/align/SimpleImageAligner.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/align/SimpleImageAligner.kt
@@ -10,8 +10,7 @@ class SimpleImageAligner(
     private val radiusY: Int = 100,
     private val fastRadiusX: Int = radiusX,
     private val fastRadiusY: Int = 0,
-    private val fastErrorThreshold: Double = 1.1,
-    private val channel: Channel = Channel.Red
+    private val fastErrorThreshold: Double = 1.5,
 ) : ImageAligner {
 
     constructor(radius: Int) : this(radius, radius)
@@ -24,8 +23,8 @@ class SimpleImageAligner(
         val baseCropped1 = base.crop(centerX-fastRadiusX, centerY-fastRadiusY, fastRadiusX*2+1, fastRadiusY*2+1, false)
         val baseCropped2 = base.crop(centerX-radiusX, centerY-radiusY, radiusX*2+1, radiusY+1, false)
 
-        var bestError1 = 1.0
-        var bestError2 = 1.0
+        var bestError1 = Double.MAX_VALUE
+        var bestError2 = Double.MAX_VALUE
 
         var bestAlignX = 0
         var bestAlignY = 0
@@ -33,12 +32,12 @@ class SimpleImageAligner(
             for (dx in -maxOffset .. maxOffset) {
                 val x = centerX + dx
                 val y = centerY + dy
-                val error1 = baseCropped1.averageError(image.crop(x-fastRadiusX, y-fastRadiusY, fastRadiusX*2+1, fastRadiusY+1, false), channel)
+                val error1 = baseCropped1.averageError(image.crop(x-fastRadiusX, y-fastRadiusY, fastRadiusX*2+1, fastRadiusY+1, false))
                 if (error1 < bestError1 * fastErrorThreshold) {
                     //println("Error1: $dx, $dy : $error1")
-                    val error2 = baseCropped2.averageError(image.crop(x-radiusX, y-radiusY, radiusX*2+1, radiusY*2+1, false), channel)
+                    val error2 = baseCropped2.averageError(image.crop(x-radiusX, y-radiusY, radiusX*2+1, radiusY*2+1, false))
                     if (error2 < bestError2) {
-                        println("Error2: $dx, $dy : $error2")
+                        //println("Error2: $dx, $dy : $error2")
                         bestAlignX = dx
                         bestAlignY = dy
                         bestError1 = error1
