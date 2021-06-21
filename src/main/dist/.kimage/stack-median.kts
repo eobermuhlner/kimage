66398bed2cb7a2916f5b138fe944daedd8e3c907
Eric Obermühlner <eobermuhlner@gmail.com>
2021-06-21 19:55:58 +0200

    add stack-median script

---
+++ src/main/dist/.kimage/stack-median.kts
@@ -0,0 +1,56 @@
+import ch.obermuhlner.kimage.*
+import ch.obermuhlner.kimage.align.*
+import ch.obermuhlner.kimage.filter.*
+import ch.obermuhlner.kimage.huge.*
+import ch.obermuhlner.kimage.image.*
+import ch.obermuhlner.kimage.io.*
+import java.io.*
+import kotlin.math.*
+
+require(multiMode)
+
+println("Stack multiple images using median")
+
+val files = inputFiles as List<File>
+val parameters = inputParameters as Map<String, String>
+
+println("Loading image: $files[0]")
+var baseImage: Image = ImageReader.readMatrixImage(files[0])
+val channels = baseImage.channels
+val huge = HugeFloatArray(File("temp.mem"), files.size, channels.size, baseImage.width, baseImage.height)
+
+for (fileIndex in files.indices) {
+    val inputFile = files[fileIndex]
+
+    val image = if (fileIndex == 0) {
+        baseImage
+    } else {
+        println("Loading image: $inputFile")
+        ImageReader.readMatrixImage(inputFile)
+    }
+
+    for (channelIndex in channels.indices) {
+        val matrix = image[channels[channelIndex]]
+        for (matrixIndex in 0 until matrix.size) {
+            huge[fileIndex, channelIndex, matrixIndex] = matrix[matrixIndex].toFloat()
+        }
+    }
+}
+
+val resultImage = MatrixImage(baseImage.width, baseImage.height, channels)
+val values = FloatArray(files.size)
+for (channelIndex in channels.indices) {
+    val matrix = baseImage[channels[channelIndex]]
+    val resultMatrix = resultImage[channels[channelIndex]]
+    for (matrixIndex in 0 until matrix.size) {
+        for (fileIndex in files.indices) {
+            values[fileIndex] = huge[fileIndex, channelIndex, matrixIndex]
+        }
+
+        values.sort()
+        val stackedValue = values[values.size / 2]
+        resultMatrix[matrixIndex] = stackedValue.toDouble()
+    }
+}
+
+resultImage
