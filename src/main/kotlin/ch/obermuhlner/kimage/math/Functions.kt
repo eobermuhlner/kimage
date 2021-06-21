a50a5d32ac4f457b4942af6727dfbd1cec163a2b
Eric Obermühlner <eobermuhlner@gmail.com>
2021-06-21 23:30:19 +0200

    first version of some array math functions

--- src/main/kotlin/ch/obermuhlner/kimage/math/Functions.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/math/Functions.kt
@@ -23,3 +23,73 @@ fun clamp(x: Int, min: Int, max: Int): Int {
         else -> x
     }
 }
+
+fun FloatArray.median(): Float {
+    return toMutableList().median()
+}
+
+fun MutableList<Float>.median(): Float {
+    if (size == 0) {
+        return Float.NaN
+    }
+    this.sort()
+
+    val half = size / 2
+    if (size % 2 == 0) {
+        return (this[half-1] + this[half]) / 2
+    } else {
+        return this[half]
+    }
+}
+
+fun DoubleArray.median(): Double {
+    return toMutableList().median()
+}
+
+fun MutableList<Double>.median(): Double {
+    if (size == 0) {
+        return Double.NaN
+    }
+
+    sort()
+
+    if (size % 2 == 0) {
+        return (this[size/2] + this[size+1]) / 2
+    } else {
+        return this[size/2]
+    }
+}
+
+fun FloatArray.stddev(): Float {
+    return toList().stddev()
+}
+
+fun List<Float>.stddev(): Float {
+    val avg = average().toFloat()
+    var sum = 0.0f
+    for (value in this) {
+        val delta = value - avg
+        sum += delta * delta
+    }
+    return if (size == 0) {
+        Float.NaN
+    } else {
+        sum / size
+    }
+}
+
+fun FloatArray.medianSigmaClip(alpha: Float = 2f, iterations: Int = 1): FloatArray {
+    var result = this.toMutableList()
+
+    for (i in 0 until iterations) {
+        val sigma = result.stddev()
+        val m = result.median()
+
+        val low = m - alpha * sigma
+        val high = m + alpha * sigma
+
+        result = this.filter { it in low..high } .toMutableList()
+    }
+
+    return result.toFloatArray()
+}
