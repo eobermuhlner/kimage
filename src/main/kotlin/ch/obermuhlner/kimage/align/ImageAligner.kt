a86b2535a32bc497b1971f377fa3bd65040189b7
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-06-22 18:21:02 +0200

    simplify ImageAligner

--- src/main/kotlin/ch/obermuhlner/kimage/align/ImageAligner.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/align/ImageAligner.kt
@@ -1,12 +1,88 @@
-package ch.obermuhlner.astro.gradient.align
+package ch.obermuhlner.kimage.align
 
+import ch.obermuhlner.kimage.averageError
+import ch.obermuhlner.kimage.image.Channel
 import ch.obermuhlner.kimage.image.Image
+import kotlin.math.max
 
-interface ImageAligner {
+class ImageAligner(
+    private val radiusX: Int = 100,
+    private val radiusY: Int = 100,
+    private val fastRadiusX: Int = radiusX,
+    private val fastRadiusY: Int = 0,
+    private val fastErrorThreshold: Double = 1.5,
+) {
 
-    fun findInterestingCropCenter(image: Image): Pair<Int, Int>
+    constructor(radius: Int) : this(radius, radius)
 
-    fun align(base: Image, image: Image, centerX: Int = base.width/2, centerY: Int = base.height/2, maxOffset: Int = 200): Alignment
+    fun findInterestingCropCenter(image: Image, insetFactor: Int = 4, stepFactor: Int = 10): Pair<Int, Int> {
+        val insetWidth = image.width / insetFactor
+        val insetHeight = image.height / insetFactor
+        val radiusStepX = max(radiusX / stepFactor, 1)
+        val radiusStepY = max(radiusY / stepFactor, 1)
+        var bestStdDev = 0.0
+        var bestX = 0
+        var bestY = 0
+        for (y in insetWidth until image.height - insetWidth step radiusStepY) {
+            for (x in insetHeight until image.width - insetHeight step radiusStepX) {
+                val croppedImage = image.cropCenter(x, y, radiusX, radiusY)
+                val stddev = croppedImage[Channel.Red].stddev()
+                if (stddev > bestStdDev) {
+                    //ImageWriter.write(croppedImage, File("check_${stddev}.png"))
+                    //println("stddev $x, $y : $stddev")
+                    bestStdDev = stddev
+                    bestX = x
+                    bestY = y
+                }
+            }
+        }
 
-    data class Alignment(val x: Int, val y: Int, val error: Double)
+        return Pair(bestX, bestY)
+    }
+
+    fun align(base: Image, image: Image, centerX: Int, centerY: Int, maxOffset: Int): Alignment {
+        if (base === image) {
+            return Alignment(0, 0, 0.0)
+        }
+
+        val baseCropped0 = base.crop(centerX, centerY, 1, 1, false)
+        val baseCropped1 = base.crop(centerX-fastRadiusX, centerY-fastRadiusY, fastRadiusX*2+1, fastRadiusY*2+1, false)
+        val baseCropped2 = base.crop(centerX-radiusX, centerY-radiusY, radiusX*2+1, radiusY+1, false)
+
+        var bestError0 = Double.MAX_VALUE
+        var bestError1 = Double.MAX_VALUE
+        var bestError2 = Double.MAX_VALUE
+
+        var bestAlignX = 0
+        var bestAlignY = 0
+        for (dy in -maxOffset .. maxOffset) {
+            for (dx in -maxOffset .. maxOffset) {
+                val x = centerX + dx
+                val y = centerY + dy
+                val crop0 = image.crop(x, y, 1, 1, false)
+                val error0 = baseCropped0.averageError(crop0)
+                if (error0 < bestError0 * fastErrorThreshold) {
+                    //println("Error0: $dx, $dy : $error0")
+                    val crop1 = image.crop(x-fastRadiusX, y-fastRadiusY, fastRadiusX*2+1, fastRadiusY+1, false)
+                    val error1 = baseCropped1.averageError(crop1)
+                    if (error1 < bestError1 * fastErrorThreshold) {
+                        //println("Error1: $dx, $dy : $error1")
+                        val crop2 = image.crop(x - radiusX, y - radiusY, radiusX * 2 + 1, radiusY * 2 + 1, false)
+                        val error2 = baseCropped2.averageError(crop2)
+                        if (error2 < bestError2) {
+                            //println("Error2: $dx, $dy : $error2")
+                            bestAlignX = dx
+                            bestAlignY = dy
+                            bestError1 = error1
+                            bestError2 = error2
+                        }
+                    }
+                }
+            }
+        }
+
+        return Alignment(bestAlignX, bestAlignY, bestError2)
+    }
 }
+
+data class Alignment(val x: Int, val y: Int, val error: Double)
