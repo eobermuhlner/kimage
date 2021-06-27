b7a61593ba88901438530cb70831da10f6a05f50
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-06-27 23:11:31 +0200

    migrate to dsl: stack.kts

--- src/main/dist/.kimage/stack.kts
+++ src/main/dist/.kimage/stack.kts
@@ -9,69 +9,103 @@ import ch.obermuhlner.kimage.math.*
 import java.io.*
 import kotlin.math.*
 
-require(multiMode)
-
-println("Stack multiple images")
-
-val files = inputFiles as List<File>
-val parameters = inputParameters as Map<String, String>
-
-val methodName = parameters.getOrDefault("method", "sigmaclip-median")
-val sigmaClipKappa = parameters.getOrDefault("kappa", "2.0").toFloat()
-val sigmaClipIterations = parameters.getOrDefault("iterations", "5").toInt()
-
-println("Parameters:")
-println("  method = $methodName")
-println("  kappa = $sigmaClipKappa")
-println("  iterations = $sigmaClipIterations")
-println()
-
-val stackingMethod: (FloatArray) -> Float = when(methodName) {
-    "median" -> { array -> array.median() }
-    "average" -> { array -> array.average() }
-    "max" -> { array -> array.maxOrNull()!! }
-    "sigmaclip-median" -> { array -> array.sigmaClip(kappa = sigmaClipKappa, iterations = sigmaClipIterations).median() }
-    "sigmaclip-average" -> { array -> array.sigmaClip(kappa = sigmaClipKappa, iterations = sigmaClipIterations).average() }
-    else -> throw IllegalArgumentException("Unknown method: " + methodName)
-}
-
-println("Loading image: $files[0]")
-var baseImage: Image = ImageReader.readMatrixImage(files[0])
-val channels = baseImage.channels
-val huge = HugeFloatArray(files.size, channels.size, baseImage.width, baseImage.height)
-
-for (fileIndex in files.indices) {
-    val inputFile = files[fileIndex]
-
-    val image = if (fileIndex == 0) {
-        baseImage
-    } else {
-        println("Loading image: $inputFile")
-        ImageReader.readMatrixImage(inputFile)
-    }
-
-    for (channelIndex in channels.indices) {
-        val matrix = image[channels[channelIndex]]
-        for (matrixIndex in 0 until matrix.size) {
-            huge[fileIndex, channelIndex, matrixIndex] = matrix[matrixIndex].toFloat()
+kimage(0.1) {
+    name = "stack"
+    description = """
+                Stacks multiple image using one of several algorithms.
+                """
+    arguments {
+        string("method") { """
+                        Method used to calculate the stacked image.
+                        
+                        The method `sigmaclip-median` removes outliers before using `median`on the remaining values.
+                        The method `sigmaclip-average` removes outliers before using `average`on the remaining values.
+                        """
+            allowed = listOf("median", "average", "max", "min", "sigmaclip-median", "sigmaclip-average")
+            default = "sigmaclip-median"
+        }
+        double("kappa") {
+            description = """
+                        The kappa factor is used in sigma-clipping to define how far from the center the outliers are allowed to be.
+                        """
+            min = 0.0
+            default = 2.0
+        }
+        int("iterations") {
+            description = """
+                        The number of iterations used in sigma-clipping to remove outliers.
+                        """
+            min = 0
+            default = 10
         }
     }
-}
 
-val resultImage = MatrixImage(baseImage.width, baseImage.height, channels)
-val values = FloatArray(files.size)
-for (channelIndex in channels.indices) {
-    val matrix = baseImage[channels[channelIndex]]
-    val resultMatrix = resultImage[channels[channelIndex]]
-    for (matrixIndex in 0 until matrix.size) {
-        for (fileIndex in files.indices) {
-            values[fileIndex] = huge[fileIndex, channelIndex, matrixIndex]
+    multi {
+        println("Stack multiple images")
+
+        val method by arguments.string
+        val kappa by arguments.double
+        val iterations by arguments.int
+
+        println("Arguments:")
+        println("  method = $method")
+        println("  kappa = $kappa")
+        println("  iterations = $iterations")
+        println()
+
+        val stackingMethod: (FloatArray) -> Float = when(method) {
+            "median" -> { array -> array.median() }
+            "average" -> { array -> array.average() }
+            "max" -> { array -> array.maxOrNull()!! }
+            "min" -> { array -> array.minOrNull()!! }
+            "sigmaclip-median" -> { array -> array.sigmaClip(kappa = kappa.toFloat(), iterations = iterations).median() }
+            "sigmaclip-average" -> { array -> array.sigmaClip(kappa = kappa.toFloat(), iterations = iterations).average() }
+            else -> throw IllegalArgumentException("Unknown method: " + method)
         }
 
-        values.sort()
-        val stackedValue = stackingMethod(values)
-        resultMatrix[matrixIndex] = stackedValue.toDouble()
+        println("Loading image: ${inputFiles[0]}")
+        var baseImage: Image = ImageReader.readMatrixImage(inputFiles[0])
+        val channels = baseImage.channels
+        val huge = HugeFloatArray(inputFiles.size, channels.size, baseImage.width, baseImage.height)
+
+        for (fileIndex in inputFiles.indices) {
+            val inputFile = inputFiles[fileIndex]
+
+            val image = if (fileIndex == 0) {
+                baseImage
+            } else {
+                println("Loading image: $inputFile")
+                ImageReader.readMatrixImage(inputFile).crop(0, 0, baseImage.width, baseImage.height)
+            }
+
+            for (channelIndex in channels.indices) {
+                val matrix = image[channels[channelIndex]]
+                for (matrixIndex in 0 until matrix.size) {
+                    huge[fileIndex, channelIndex, matrixIndex] = matrix[matrixIndex].toFloat()
+                }
+            }
+        }
+        println()
+
+        println("Stacking ${inputFiles.size} images using $method")
+        val resultImage = MatrixImage(baseImage.width, baseImage.height, channels)
+        val values = FloatArray(inputFiles.size)
+        for (channelIndex in channels.indices) {
+            val channel = channels[channelIndex]
+            print("Stacking channel: $channel")
+            val matrix = baseImage[channel]
+            val resultMatrix = resultImage[channel]
+            for (matrixIndex in 0 until matrix.size) {
+                for (fileIndex in inputFiles.indices) {
+                    values[fileIndex] = huge[fileIndex, channelIndex, matrixIndex]
+                }
+
+                val stackedValue = stackingMethod(values)
+                resultMatrix[matrixIndex] = stackedValue.toDouble()
+            }
+        }
+        println()
+
+        resultImage
     }
 }
-
-resultImage
--- src/test/kotlin/TestScript.kt
+++ src/test/kotlin/TestScript.kt
@@ -1,7 +1,11 @@
 import ch.obermuhlner.kimage.*
 import ch.obermuhlner.kimage.align.*
+import ch.obermuhlner.kimage.huge.HugeFloatArray
 import ch.obermuhlner.kimage.image.*
 import ch.obermuhlner.kimage.io.*
+import ch.obermuhlner.kimage.math.average
+import ch.obermuhlner.kimage.math.median
+import ch.obermuhlner.kimage.math.sigmaClip
 
 import java.io.*
 import kotlin.math.*
@@ -11,7 +15,8 @@ object TestScript {
     @JvmStatic
     fun main(args: Array<String>) {
         //runScript(scriptAlign(), "images/align/orion1.png", "images/align/orion2.png", "images/animal.png")
-        runScript(scriptRemoveBackgroundMedian(), "images/align/orion1.png", "images/align/orion2.png", "images/animal.png")
+        //runScript(scriptRemoveBackgroundMedian(), "images/align/orion1.png", "images/align/orion2.png", "images/animal.png")
+        runScript(scriptStack(), "images/align/orion1.png", "images/align/orion2.png", "images/animal.png")
     }
 
     private fun scriptRemoveBackgroundMedian(): Script =
@@ -212,6 +217,110 @@ object TestScript {
                 }
             }
         }
+
+    fun scriptStack() =
+        kimage(0.1) {
+            name = "stack"
+            description = """
+                Stacks multiple image using one of several algorithms.
+                """
+            arguments {
+                string("method") { """
+                        Method used to calculate the stacked image.
+                        
+                        The method `sigmaclip-median` removes outliers before using `median`on the remaining values.
+                        The method `sigmaclip-average` removes outliers before using `average`on the remaining values.
+                        """
+                    allowed = listOf("median", "average", "max", "min", "sigmaclip-median", "sigmaclip-average")
+                    default = "sigmaclip-median"
+                }
+                double("kappa") {
+                    description = """
+                        The kappa factor is used in sigma-clipping to define how far from the center the outliers are allowed to be.
+                        """
+                    min = 0.0
+                    default = 2.0
+                }
+                int("iterations") {
+                    description = """
+                        The number of iterations used in sigma-clipping to remove outliers.
+                        """
+                    min = 0
+                    default = 10
+                }
+            }
+
+            multi {
+                println("Stack multiple images")
+
+                val method by arguments.string
+                val kappa by arguments.double
+                val iterations by arguments.int
+
+                println("Arguments:")
+                println("  method = $method")
+                println("  kappa = $kappa")
+                println("  iterations = $iterations")
+                println()
+
+                val stackingMethod: (FloatArray) -> Float = when(method) {
+                    "median" -> { array -> array.median() }
+                    "average" -> { array -> array.average() }
+                    "max" -> { array -> array.maxOrNull()!! }
+                    "min" -> { array -> array.minOrNull()!! }
+                    "sigmaclip-median" -> { array -> array.sigmaClip(kappa = kappa.toFloat(), iterations = iterations).median() }
+                    "sigmaclip-average" -> { array -> array.sigmaClip(kappa = kappa.toFloat(), iterations = iterations).average() }
+                    else -> throw IllegalArgumentException("Unknown method: " + method)
+                }
+
+                println("Loading image: ${inputFiles[0]}")
+                var baseImage: Image = ImageReader.readMatrixImage(inputFiles[0])
+                val channels = baseImage.channels
+                val huge = HugeFloatArray(inputFiles.size, channels.size, baseImage.width, baseImage.height)
+
+                for (fileIndex in inputFiles.indices) {
+                    val inputFile = inputFiles[fileIndex]
+
+                    val image = if (fileIndex == 0) {
+                        baseImage
+                    } else {
+                        println("Loading image: $inputFile")
+                        ImageReader.readMatrixImage(inputFile).crop(0, 0, baseImage.width, baseImage.height)
+                    }
+
+                    for (channelIndex in channels.indices) {
+                        val matrix = image[channels[channelIndex]]
+                        for (matrixIndex in 0 until matrix.size) {
+                            huge[fileIndex, channelIndex, matrixIndex] = matrix[matrixIndex].toFloat()
+                        }
+                    }
+                }
+                println()
+
+                println("Stacking ${inputFiles.size} images using $method")
+                val resultImage = MatrixImage(baseImage.width, baseImage.height, channels)
+                val values = FloatArray(inputFiles.size)
+                for (channelIndex in channels.indices) {
+                    val channel = channels[channelIndex]
+                    print("Stacking channel: $channel")
+                    val matrix = baseImage[channel]
+                    val resultMatrix = resultImage[channel]
+                    for (matrixIndex in 0 until matrix.size) {
+                        for (fileIndex in inputFiles.indices) {
+                            values[fileIndex] = huge[fileIndex, channelIndex, matrixIndex]
+                        }
+
+                        val stackedValue = stackingMethod(values)
+                        resultMatrix[matrixIndex] = stackedValue.toDouble()
+                    }
+                }
+                println()
+
+                resultImage
+            }
+        }
+
+
 
     fun runScript(script: Script, vararg filepaths: String) {
         runScript(script, mapOf(), *filepaths)
