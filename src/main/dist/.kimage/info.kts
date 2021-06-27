bb1f753f6f30ce43dffdcb2ae65d4b943b0f9de5
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-06-27 21:00:17 +0200

    migrate to dsl: info.kts

--- src/main/dist/.kimage/info.kts
+++ src/main/dist/.kimage/info.kts
@@ -7,16 +7,22 @@ import java.io.*
 import java.nio.file.*
 import kotlin.math.*
 
-require(multiMode)
+kimage(0.1) {
+    name = "info"
+    description = """
+                Print info about images.
+                """
+    arguments {
+    }
 
-val files = inputFiles as List<File>
-val parameters = inputParameters as Map<String, String>
-
-println(String.format("%-40s %6s %6s %12s", "Name", "Exists", "Type", "Bytes"))
-for (file in files) {
-    val fileSize = if (file.exists()) Files.size(file.toPath()) else 0
-    val fileType = if (file.isFile()) "File" else if (file.isDirectory()) "Dir" else "Other"
-    println(String.format("%-40s %6s %6s %12d", file.name, file.exists(), fileType, fileSize))
+    multi {
+        println(String.format("%-40s %6s %6s %12s", "Name", "Exists", "Type", "Bytes"))
+        for (file in inputFiles) {
+            val fileSize = if (file.exists()) Files.size(file.toPath()) else 0
+            val fileType = if (file.isFile()) "File" else if (file.isDirectory()) "Dir" else "Other"
+            println(String.format("%-40s %6s %6s %12d", file.name, file.exists(), fileType, fileSize))
+        }
+    }
 }
 
 
