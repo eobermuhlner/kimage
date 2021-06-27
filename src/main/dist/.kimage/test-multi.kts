9d04f79aaa07e20c43a6a9f08cc621de0dfac601
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-06-27 21:40:59 +0200

    migrate to dsl: test-multi.kts

--- src/main/dist/.kimage/test-multi.kts
+++ src/main/dist/.kimage/test-multi.kts
@@ -6,22 +6,75 @@ import ch.obermuhlner.kimage.io.*
 import java.io.*
 import kotlin.math.*
 
-require(multiMode)
+kimage(0.1) {
+    name = "test-multi"
+    description = """
+                Test script to show how to handle multiple images in a kimage script.
+                """
+    arguments {
+        int("intArg") {
+            description = "Example argument for an int value."
+            min = 0
+            max = 100
+            default = 0
+        }
+        double("doubleArg") {
+            description = "Example argument for a double value."
+            min = 0.0
+            max = 100.0
+            default = 50.0
+        }
+        boolean("booleanArg") {
+            description = "Example argument for a boolean value."
+            default = false
+        }
+        string("stringArg") {
+            description = "Example argument for a string value."
+            default = "undefined"
+        }
+        string("allowedStringArg") {
+            description = "Example argument for a string value with some allowed strings."
+            allowed = listOf("red", "green", "blue")
+            default = "red"
+        }
+        string("regexStringArg") {
+            description = "Example argument for a string value with regular expression."
+            regex = "a+"
+            default = "aaa"
+        }
+    }
 
-println("Test multi image script")
+    multi {
+        val intArg by arguments.int
+        val doubleArg by arguments.double
+        val booleanArg by arguments.boolean
+        val stringArg by arguments.string
+        val allowedStringArg by arguments.string
+        val regexStringArg by arguments.string
 
-val files = inputFiles as List<File>
-val parameters = inputParameters as Map<String, String>
+        println("Test multi image script")
+        println()
 
-println("Input files: $files")
-for (parameter in parameters) {
-    val key: String = parameter.key
-    val value: String = parameter.value
-    println("  Parameter: ${key} = ${value}")
+        println("Raw Arguments:")
+        for (rawArgument in rawArguments) {
+            val key: String = rawArgument.key
+            val value: String = rawArgument.value
+            println("  Argument: ${key} = ${value}")
+        }
+        println()
+
+        println("Arguments:")
+        println("  intArg = $intArg")
+        println("  doubleArg = $doubleArg")
+        println("  booleanArg = $booleanArg")
+        println("  stringArg = $stringArg")
+        println("  allowedStringArg = $allowedStringArg")
+        println("  regexStringArg = $regexStringArg")
+        println()
+
+        println("Input Files:")
+        for (file in inputFiles) {
+            println("  File: $file exists=${file.exists()}")
+        }
+    }
 }
-
-for (file in files) {
-    println("  File: $file ${file.exists()}")
-}
-
-null
