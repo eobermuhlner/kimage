c2efeebbc93b39aa4d2f83ce7c7dfea242097c0c
Eric Obermühlner <eobermuhlner@gmail.com>
2021-06-29 22:36:02 +0200

    unit test Matrix.scaleTo() using Nearest

--- src/test/kotlin/ch/obermuhlner/kimage/matrix/MatrixFunctionsTest.kt
+++ src/test/kotlin/ch/obermuhlner/kimage/matrix/MatrixFunctionsTest.kt
@@ -1,14 +1,23 @@
 package ch.obermuhlner.kimage.matrix
 
 import ch.obermuhlner.kimage.Scaling
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class MatrixFunctionsTest {
 
     @Test
-    fun testScaleTo() {
-        val m = Matrix.matrixOf(2, 2, 0.1, 0.2, 0.3, 0.4)
-        val m2 = m.scaleTo(4, 4, Scaling.Nearest)
-        println(m2)
+    fun testScaleToNearest() {
+        val matrix = Matrix.matrixOf(2, 2,
+            0.1, 0.2,
+            0.3, 0.4)
+        val scaled = matrix.scaleTo(4, 4, Scaling.Nearest)
+
+        val expected = Matrix.matrixOf(4, 4,
+            0.1, 0.1, 0.2, 0.2,
+            0.1, 0.1, 0.2, 0.2,
+            0.3, 0.3, 0.4, 0.4,
+            0.3, 0.3, 0.4, 0.4)
+        assertEquals(expected, scaled)
     }
 }
