fb1f72892c4a3f9bf02211ec0142e203b4e2a6d8
Eric Obermühlner <eobermuhlner@gmail.com>
2021-07-02 16:45:05 +0200

    use Image.histogramImage()

--- src/main/dist/.kimage/histogram.kts
+++ src/main/dist/.kimage/histogram.kts
@@ -36,41 +36,11 @@ kimage(0.1) {
         val width: Int by arguments
         val height: Int by arguments
 
-        val channels = listOf(Channel.Red, Channel.Green, Channel.Blue)
-
         println("Arguments:")
         println("  width = $width")
         println("  height = $height")
         println()
 
-        val channelHistograms = mutableMapOf<Channel, Histogram>()
-        var maxCount = 0
-        for (channel in channels) {
-            val histogram = Histogram(width)
-            channelHistograms[channel] = histogram
-
-            inputImage[channel].forEach { histogram.add(it) }
-            maxCount = max(maxCount, histogram.max())
-        }
-
-        val output = MatrixImage(width, height)
-
-        for (channel in channels) {
-            val histogram = channelHistograms[channel]!!
-            if (verboseMode) {
-                println("Histogram $channel")
-                histogram.print()
-                println()
-            }
-
-            for (x in 0 until width) {
-                val histY = (height.toDouble() * histogram[x] / maxCount).toInt()
-                for (y in (height-histY) until height) {
-                    output[channel][y, x] = 1.0
-                }
-            }
-        }
-
-        output
+        inputImage.histogramImage(width, height)
     }
 }
