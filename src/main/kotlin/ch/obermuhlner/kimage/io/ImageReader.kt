18b9b44fcfc93df96685b86308c2050351a47939
Eric Obermühlner <eobermuhlner@gmail.com>
2021-07-03 11:59:51 +0200

    improve ImageIO image conversion

--- src/main/kotlin/ch/obermuhlner/kimage/io/ImageReader.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/io/ImageReader.kt
@@ -2,7 +2,8 @@ package ch.obermuhlner.kimage.io
 
 import ch.obermuhlner.kimage.image.Image
 import ch.obermuhlner.kimage.image.MatrixImage
-import ch.obermuhlner.kimage.image.awt.AwtBufferedImage
+import java.awt.image.BufferedImage
+import java.awt.image.DataBuffer
 import java.io.File
 import javax.imageio.ImageIO
 
@@ -10,7 +11,47 @@ object ImageReader {
 
     fun read(file: File): Image {
         val image = ImageIO.read(file) ?: throw RuntimeException("Failed to read image: $file")
-        return AwtBufferedImage(image)
+
+        val color = DoubleArray(3)
+        val matrixImage = MatrixImage(image.width, image.height)
+        if (image.type == BufferedImage.TYPE_CUSTOM) {
+            // TODO check colorModel and convert non-RGB correctly
+            for (y in 0 until image.height) {
+                for (x in 0 until image.width) {
+                    image.raster.getPixel(x, y, color)
+
+                    when (image.raster.transferType) {
+                        DataBuffer.TYPE_USHORT -> for (i in color.indices) {
+                            color[i] = color[i]/UShort.MAX_VALUE.toDouble()
+                        }
+                        DataBuffer.TYPE_SHORT -> for (i in color.indices) {
+                            color[i] = color[i]/Short.MAX_VALUE.toDouble()
+                        }
+                        DataBuffer.TYPE_INT -> for (i in color.indices) {
+                            color[i] = color[i]/Int.MAX_VALUE.toDouble()
+                        }
+                        DataBuffer.TYPE_BYTE -> for (i in color.indices) {
+                            color[i] = color[i]/Byte.MAX_VALUE.toDouble()
+                        }
+                    }
+
+                    matrixImage.setPixel(x, y, color)
+                }
+            }
+        } else {
+            for (y in 0 until image.height) {
+                for (x in 0 until image.width) {
+                    val argb = image.getRGB(x, y)
+                    //alpha = (argb shr 24 and 0xff) / 255.0
+                    color[0] = (argb shr 16 and 0xff) / 255.0
+                    color[1] = (argb shr 8 and 0xff) / 255.0
+                    color[2] = (argb and 0xff) / 255.0
+                    matrixImage.setPixel(x, y, color)
+                }
+            }
+        }
+
+        return matrixImage
     }
 
     fun readMatrixImage(file: File): MatrixImage {
--- src/main/kotlin/ch/obermuhlner/kimage/io/ImageWriter.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/io/ImageWriter.kt
@@ -1,9 +1,14 @@
 package ch.obermuhlner.kimage.io
 
 import ch.obermuhlner.kimage.image.Image
-import ch.obermuhlner.kimage.image.awt.AwtBufferedImage
+import ch.obermuhlner.kimage.image.onEach
+import ch.obermuhlner.kimage.math.clamp
+import java.awt.Transparency
+import java.awt.color.ColorSpace
+import java.awt.image.*
 import java.io.File
 import javax.imageio.ImageIO
+import javax.imageio.ImageTypeSpecifier
 
 object ImageWriter {
     fun write(image: Image, output: File) {
@@ -20,13 +25,60 @@ object ImageWriter {
     }
 
     fun write(image: Image, output: File, format: ImageFormat) {
-        when(image)  {
-            is AwtBufferedImage -> {
-                ImageIO.write(image.bufferedImage, format.name, output)
-            }
-            else -> {
-                write(AwtBufferedImage.from(image), output, format)
+        image.onEach { v -> clamp(v, 0.0, 1.0) }
+
+        val bufferedImage = createBufferedImageUShort(image.width, image.height)
+
+        val color = DoubleArray(3)
+        for (y in 0 until image.height) {
+            for (x in 0 until image.width) {
+                image.getPixel(x, y, color)
+
+                when (bufferedImage.raster.transferType) {
+                    DataBuffer.TYPE_USHORT -> for (i in color.indices) {
+                        color[i] = color[i] * UShort.MAX_VALUE.toDouble()
+                    }
+                    DataBuffer.TYPE_SHORT -> for (i in color.indices) {
+                        color[i] = color[i] * Short.MAX_VALUE.toDouble()
+                    }
+                    DataBuffer.TYPE_INT -> for (i in color.indices) {
+                        color[i] = color[i] * Int.MAX_VALUE.toDouble()
+                    }
+                    DataBuffer.TYPE_BYTE -> for (i in color.indices) {
+                        color[i] = color[i] * Byte.MAX_VALUE.toDouble()
+                    }
+                }
+
+                bufferedImage.raster.setPixel(x, y, color)
             }
         }
+
+        ImageIO.write(bufferedImage, format.name, output)
+    }
+
+    private fun createBufferedImageIntRGB(width: Int, height: Int): BufferedImage {
+        return BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    }
+
+    private fun createBufferedImageUShort(width: Int, height: Int): BufferedImage {
+        val specifier = ImageTypeSpecifier.createInterleaved(
+            ColorSpace.getInstance(ColorSpace.CS_sRGB),
+            intArrayOf(0, 1, 2),
+            DataBuffer.TYPE_USHORT,
+            false,
+            false
+        )
+        return specifier.createBufferedImage(width, height)
+    }
+
+    private fun createBufferedImageFloat(width: Int, height: Int): BufferedImage {
+        val specifier = ImageTypeSpecifier.createInterleaved(
+            ColorSpace.getInstance(ColorSpace.CS_sRGB),
+            intArrayOf(0, 1, 2),
+            DataBuffer.TYPE_FLOAT,
+            false,
+            false
+        )
+        return specifier.createBufferedImage(width, height)
     }
 }
