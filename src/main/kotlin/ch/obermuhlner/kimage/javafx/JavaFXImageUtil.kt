77344d4223d01873ffa29aeaf2fb23c5c754f815
Eric Obermühlner <eobermuhlner@gmail.com>
2021-07-24 12:20:43 +0200

    add copy() function

--- src/main/kotlin/ch/obermuhlner/kimage/javafx/JavaFXImageUtil.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/javafx/JavaFXImageUtil.kt
@@ -4,6 +4,7 @@ import ch.obermuhlner.kimage.image.Image
 import ch.obermuhlner.kimage.math.clamp
 import javafx.scene.image.WritableImage
 import javafx.scene.paint.Color
+import kotlin.math.min
 
 object JavaFXImageUtil {
 
@@ -20,5 +21,30 @@ object JavaFXImageUtil {
             }
         }
         return output
+    }
+
+    fun copy(source: Image, target: WritableImage) {
+        val color = DoubleArray(3)
+        val width = min(source.width, target.width.toInt())
+        val height = min(source.height, target.height.toInt())
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                source.getPixel(x, y, color)
+                color[0] = clamp(color[0], 0.0, 1.0)
+                color[1] = clamp(color[1], 0.0, 1.0)
+                color[2] = clamp(color[2], 0.0, 1.0)
+                target.pixelWriter.setColor(x, y, Color(color[0], color[1], color[2], 1.0))
+            }
+        }
+    }
+
+    fun copy(source: WritableImage, target: WritableImage) {
+        val width = min(source.width, target.width).toInt()
+        val height = min(source.height, target.height).toInt()
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                target.pixelWriter.setArgb(x, y, source.pixelReader.getArgb(x, y))
+            }
+        }
     }
 }
