8dbfcf5f19ee107d7f06b74a9632ee63d8bc0eed
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2021-07-29 20:12:14 +0200

    document convert-raw-pure

--- src/main/dist/.kimage/convert-raw-pure.kts
+++ src/main/dist/.kimage/convert-raw-pure.kts
@@ -18,27 +18,50 @@ kimage(0.1) {
                 """
     arguments {
         string("dcraw") {
+            description = """
+               The `dcraw` executable.
+               
+               If the executable is not in the `PATH` then the absolute path is required to run it.
+            """
             default = "dcraw"
         }
-        string("interpolation") {
-            allowed = listOf("none", "none-unscaled", "none-uncropped")
-            default = "none-unscaled"
+        string("option") {
+            description = """
+                Specifies the transformation options to use.
+                No demosaicing interpolation will be used.
+                
+                - `scaled`, `none`: No interpolation, with automatic scaling to fill the value range.
+                
+                  Corresponds to the `-d` option in the `dcraw` command line tool.
+                - `unscaled`, `none-unscaled`: No interpolation, no scaling.
+                
+                  Corresponds to the `-D` option in the `dcraw` command line tool.
+                - `uncropped`, `none-uncropped`: No interpolation, no scaling, no cropping.
+                
+                  Corresponds to the `-E` option in the `dcraw` command line tool.
+                """
+            allowed = listOf("scaled", "unscaled", "uncropped", "none", "none-unscaled", "none-uncropped")
+            default = "unscaled"
         }
     }
 
     fun dcraw(
         dcraw: String,
-        interpolation: String,
+        option: String,
         file: File
     ) {
         val processBuilder = ProcessBuilder()
 
         val command = mutableListOf(dcraw, "-T", "-v")
 
-        when(interpolation) {
+        when(option) {
+            "scaled" -> command.add("-d")
             "none" -> command.add("-d")
+            "unscaled" -> command.add("-D")
             "none-unscaled" -> command.add("-D")
+            "uncropped" -> command.add("-E")
             "none-uncropped" -> command.add("-E")
+            else -> throw IllegalArgumentException("Unknown option: $option")
         }
 
         command.add("-4")
@@ -60,11 +83,11 @@ kimage(0.1) {
 
     multi {
         val dcraw: String by arguments
-        val interpolation: String by arguments
+        val option: String by arguments
 
         for (inputFile in inputFiles) {
             println("Converting $inputFile")
-            dcraw(dcraw, interpolation, inputFile)
+            dcraw(dcraw, option, inputFile)
             println()
         }
 
