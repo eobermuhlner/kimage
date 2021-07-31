77b36dac40c989c1a3c35aa0d9971d21956a4002
Eric Obermühlner <eobermuhlner@gmail.com>
2021-07-31 15:29:40 +0200

    add invert script

---
+++ src/main/dist/.kimage/invert.kts
@@ -0,0 +1,26 @@
+import ch.obermuhlner.kimage.*
+import ch.obermuhlner.kimage.align.*
+import ch.obermuhlner.kimage.filter.*
+import ch.obermuhlner.kimage.image.*
+import ch.obermuhlner.kimage.io.*
+import ch.obermuhlner.kimage.math.*
+import ch.obermuhlner.kimage.matrix.*
+import java.io.*
+import java.util.*
+import kotlin.math.*
+
+kimage(0.1) {
+    name = "invert"
+    title = "Invert an image"
+    description = """
+                Invert an image.
+                """
+    arguments {
+    }
+
+    single {
+        MatrixImage(inputImage.width, inputImage.height, inputImage.channels) { channel, _, _ ->
+            inputImage[channel].onEach { _, _, value -> 1.0 - value }
+        }
+    }
+}
