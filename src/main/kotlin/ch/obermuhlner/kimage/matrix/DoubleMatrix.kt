d1098d00fef70b4fbad46bbf6382939ad7974a25
Eric Obermühlner <eobermuhlner@gmail.com>
2021-08-02 10:37:27 +0200

    add constructor to fill matrix by lambda

--- src/main/kotlin/ch/obermuhlner/kimage/matrix/DoubleMatrix.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/matrix/DoubleMatrix.kt
@@ -3,6 +3,14 @@ package ch.obermuhlner.kimage.matrix
 class DoubleMatrix(override val rows: Int, override val columns: Int) : Matrix {
     private val data: DoubleArray = DoubleArray(columns * rows)
 
+    constructor(rows: Int, columns: Int, init: (row: Int, column: Int) -> Double): this(rows, columns) {
+        for (row in 0 until rows) {
+            for (column in 0 until columns) {
+                this[row, column] = init(row, column)
+            }
+        }
+    }
+
     override fun create(createRows: Int, createColumns: Int): Matrix = DoubleMatrix(createRows, createColumns)
 
     override fun get(index: Int): Double {
