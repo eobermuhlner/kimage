b65e5e3dcbb170b469db087c5b4edb69df364a1f
Eric Obermühlner <eobermuhlner@gmail.com>
2021-08-02 10:36:49 +0200

    add blur3x3, blur5x5, blur7x7 filters

--- src/main/dist/.kimage/filter.kts
+++ src/main/dist/.kimage/filter.kts
@@ -18,6 +18,9 @@ kimage(0.1) {
                 The filter algorithm.
                 
                 - `blur` uses a gaussian blur filter of the specified `radius`
+                - `blur3x3` uses a gaussian blur filter using a 3x3 kernel
+                - `blur5x5` uses a gaussian blur filter using a 5x5 kernel
+                - `blur7x7` uses a gaussian blur filter using a 7x7 kernel
                 - `median` uses a median filter of the specified `radius`
                 - `average` uses an average filter of the specified `radius`
                 - `sharpen` uses a 3x3 sharpen mask
@@ -26,7 +29,7 @@ kimage(0.1) {
                 - `edgeDetectionCross` detects edges along the horizontal/vertial axes using a 3x3 kernel
                 - `edgeDetectionDiagonal` detects edges along both diagonals using a 3x3 kernel
                 """
-            allowed = listOf("blur", "median", "average", "sharpen", "unsharpMask", "edgeDetectionStrong", "edgeDetectionCross", "edgeDetectionDiagonal")
+            allowed = listOf("blur", "blur3x3", "blur5x5", "blur7x7", "median", "average", "sharpen", "unsharpMask", "edgeDetectionStrong", "edgeDetectionCross", "edgeDetectionDiagonal")
         }
         int("radius") {
             description = """
@@ -48,6 +51,9 @@ kimage(0.1) {
             "average" -> inputImage.averageFilter(radius)
             "sharpen" -> inputImage.sharpenFilter()
             "unsharpMask" -> inputImage.unsharpMaskFilter()
+            "blur3x3" -> inputImage.kernelFilter(KernelFilter.GaussianBlur3)
+            "blur5x5" -> inputImage.kernelFilter(KernelFilter.GaussianBlur5)
+            "blur7x7" -> inputImage.kernelFilter(KernelFilter.GaussianBlur7)
             "edgeDetectionStrong" -> inputImage.edgeDetectionStrongFilter()
             "edgeDetectionCross" -> inputImage.edgeDetectionCrossFilter()
             "edgeDetectionDiagonal" -> inputImage.edgeDetectionDiagonalFilter()
