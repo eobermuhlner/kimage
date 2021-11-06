c4060df9248ead178b09c9e7248cce9c7b6416cb
Eric Obermühlner <eobermuhlner@gmail.com>
2021-11-06 11:53:14 +0100

    docu for distance-to-median script

--- src/main/dist/.kimage/distance-to-median.kts
+++ src/main/dist/.kimage/distance-to-median.kts
@@ -15,18 +15,32 @@ kimage(0.1) {
     description = """
                 Creates a false color image showing the distance to the median.
                 
+                The difference is color coded:
+                  - black = no difference to median 
+                  - blue  = pixel has a value that is higher than the median value of the entire image
+                  - red   = pixel has a value that is lower than the median value of the entire image
+
+                The `factor` argument controls how much the differences are exaggerated.
+
                 Useful to find dirt on the sensor in a flat image.
                 Useful to visualize light pollution and vignetting in an image.
                 """
     arguments {
         string("channel") {
             description = """
-                        """
+                The channel to measure the distance.
+                """
             allowed = listOf("gray", "luminance", "red", "green", "blue")
             default = "gray"
         }
         double("factor") {
+            description = """
+                Controls how much the distances are exaggerated.
+                
+                A value of 1.0 means to not exaggerate
+                """
             default = 2.0
+            min = 0
         }
     }
 
