d3c14b8483235af029a13c84c256526f209f171b
Eric Obermühlner <eobermuhlner@gmail.com>
2021-11-07 14:49:51 +0100

    filter script to support different channels

--- src/main/dist/.kimage/filter.kts
+++ src/main/dist/.kimage/filter.kts
@@ -3,6 +3,8 @@ import ch.obermuhlner.kimage.align.*
 import ch.obermuhlner.kimage.filter.*
 import ch.obermuhlner.kimage.image.*
 import ch.obermuhlner.kimage.io.*
+import ch.obermuhlner.kimage.math.*
+import ch.obermuhlner.kimage.matrix.*
 import java.io.*
 import kotlin.math.*
 
@@ -49,29 +51,100 @@ kimage(0.1) {
             max = 1.0
             default = 0.5
         }
+        string("channels") {
+            allowed = listOf("rgb", "red", "green", "blue", "hue", "saturation", "brightness", "hsb", "hs", "hb", "sb")
+            default = "rgb"
+        }
     }
 
     single {
         val filter: String by arguments
         val radius: Int by arguments
         val strength: Double by arguments
+        val channels: String by arguments
 
-        when (filter) {
-            "blur" -> inputImage.gaussianBlurFilter(radius)
-            "median" -> inputImage.medianFilter(radius)
-            "average" -> inputImage.averageFilter(radius)
-            "sharpen" -> inputImage.sharpenFilter()
-            "unsharpMask" -> inputImage.unsharpMaskFilter(radius, strength)
-            "unsharpMask3x3" -> inputImage.kernelFilter(KernelFilter.UnsharpMask)
-            "blur3x3" -> inputImage.kernelFilter(KernelFilter.GaussianBlur3)
-            "blur5x5" -> inputImage.kernelFilter(KernelFilter.GaussianBlur5)
-            "blur7x7" -> inputImage.kernelFilter(KernelFilter.GaussianBlur7)
-            "edgeDetectionStrong3x3" -> inputImage.edgeDetectionStrongFilter()
-            "edgeDetectionCross3x3" -> inputImage.edgeDetectionCrossFilter()
-            "edgeDetectionDiagonal3x3" -> inputImage.edgeDetectionDiagonalFilter()
-            "sobel3x3" -> inputImage.sobelFilter3()
-            "sobel5x5" -> inputImage.sobelFilter5()
-            else -> throw IllegalArgumentException("Unknown filter: $filter")
+        fun matrixFilter(matrix: Matrix): Matrix {
+            return when (filter) {
+                "blur" -> matrix.gaussianBlurFilter(radius)
+                "median" -> matrix.medianFilter(radius)
+                "average" -> matrix.averageFilter(radius)
+                "sharpen" -> matrix.sharpenFilter()
+                "unsharpMask" -> matrix.unsharpMaskFilter(radius, strength)
+                "unsharpMask3x3" -> matrix.convolute(KernelFilter.UnsharpMask)
+                "blur3x3" -> matrix.convolute(KernelFilter.GaussianBlur3)
+                "blur5x5" -> matrix.convolute(KernelFilter.GaussianBlur5)
+                "blur7x7" -> matrix.convolute(KernelFilter.GaussianBlur7)
+                "edgeDetectionStrong3x3" -> matrix.edgeDetectionStrongFilter()
+                "edgeDetectionCross3x3" -> matrix.edgeDetectionCrossFilter()
+                "edgeDetectionDiagonal3x3" -> matrix.edgeDetectionDiagonalFilter()
+                "sobel3x3" -> matrix.sobelFilter3()
+                "sobel5x5" -> matrix.sobelFilter5()
+                else -> throw IllegalArgumentException("Unknown filter: $filter")
+            }
+        }
+
+        when(channels) {
+            "rgb", "red", "green", "blue" -> {
+                var red = inputImage[Channel.Red]
+                var green = inputImage[Channel.Green]
+                var blue = inputImage[Channel.Blue]
+
+                when(channels) {
+                    "red" -> red = matrixFilter(red)
+                    "green" -> green = matrixFilter(green)
+                    "blue" -> blue = matrixFilter(blue)
+                    "rgb" -> {
+                        red = matrixFilter(red)
+                        green = matrixFilter(green)
+                        blue = matrixFilter(blue)
+                    }
+                    else -> throw IllegalArgumentException("Unknown channels: $channels")
+                }
+
+                MatrixImage(inputImage.width, inputImage.height,
+                    Channel.Red to red,
+                    Channel.Green to green,
+                    Channel.Blue to blue)
+            }
+            else -> {
+                var hue = inputImage[Channel.Hue]
+                var saturation = inputImage[Channel.Saturation]
+                var brightness = inputImage[Channel.Brightness]
+
+                when(channels) {
+                    "hue" -> hue = matrixFilter(hue)
+                    "saturation" -> saturation = matrixFilter(saturation)
+                    "brightness" -> brightness = matrixFilter(brightness)
+                    "hsb" -> {
+                        hue = matrixFilter(hue)
+                        saturation = matrixFilter(saturation)
+                        brightness = matrixFilter(brightness)
+                    }
+                    "hs" -> {
+                        hue = matrixFilter(hue)
+                        saturation = matrixFilter(saturation)
+                    }
+                    "hb" -> {
+                        hue = matrixFilter(hue)
+                        brightness = matrixFilter(brightness)
+                    }
+                    "sb" -> {
+                        saturation = matrixFilter(saturation)
+                        brightness = matrixFilter(brightness)
+                    }
+                    else -> throw IllegalArgumentException("Unknown channels: $channels")
+                }
+
+                val hsbImage = MatrixImage(inputImage.width, inputImage.height,
+                    Channel.Hue to hue,
+                    Channel.Saturation to saturation,
+                    Channel.Brightness to brightness)
+
+                MatrixImage(inputImage.width, inputImage.height,
+                    Channel.Red to hsbImage[Channel.Red],
+                    Channel.Green to hsbImage[Channel.Green],
+                    Channel.Blue to hsbImage[Channel.Blue])
+            }
         }
     }
 }
