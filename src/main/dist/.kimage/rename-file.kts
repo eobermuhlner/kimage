06f6fa0e12a1ec83dc81e26d69c79f2e58e072a7
Eric Obermühlner <eobermuhlner@gmail.com>
2022-03-04 16:18:00 +0100

    add rename-file script

---
+++ src/main/dist/.kimage/rename-file.kts
@@ -0,0 +1,40 @@
+import ch.obermuhlner.kimage.*
+import ch.obermuhlner.kimage.align.*
+import ch.obermuhlner.kimage.filter.*
+import ch.obermuhlner.kimage.image.*
+import ch.obermuhlner.kimage.io.*
+import ch.obermuhlner.kimage.math.*
+import ch.obermuhlner.kimage.matrix.*
+import java.io.*
+import java.nio.file.Files
+import java.util.*
+import kotlin.math.*
+
+kimage(0.1) {
+    name = "rename-file"
+    title = "Copy and rename files into another directory"
+    description = """
+                Rename images.
+                """
+    arguments {
+        file("target") {
+            description = """
+                The target directory to move the image file into.
+                """
+            isDirectory = true
+        }
+    }
+
+    multi {
+        val target: File by arguments
+
+        if (!target.exists()) {
+            target.mkdirs()
+        }
+
+        for (inputFile in inputFiles) {
+            println("Moving $inputFile into $target directory")
+            Files.move(inputFile.toPath(), File(target, inputFile.name).toPath())
+        }
+    }
+}
