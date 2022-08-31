13279100e8d4cab66528baede565596947fe6c81
Eric Obermühlner <eobermuhlner@gmail.com>
2022-08-31 21:06:03 +0200

    experimental AI with deeplearning4j

--- src/main/kotlin/ch/obermuhlner/kimage/ai/ImagePixelAI.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/ai/ImagePixelAI.kt
@@ -1,60 +1,71 @@
 package ch.obermuhlner.kimage.ai
 
-import ch.obermuhlner.kimage.image.Channel
-import ch.obermuhlner.kimage.image.Image
-import ch.obermuhlner.kimage.image.MatrixImage
-import ch.obermuhlner.kimage.image.crop
+import ch.obermuhlner.kimage.image.*
 import ch.obermuhlner.kimage.io.ImageReader
+import ch.obermuhlner.kimage.math.clamp
 import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration
 import org.deeplearning4j.nn.conf.layers.DenseLayer
 import org.deeplearning4j.nn.conf.layers.OutputLayer
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
-import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.deeplearning4j.util.ModelSerializer
 import org.nd4j.linalg.activations.Activation
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.dataset.DataSet
 import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.learning.config.Adam
+import org.nd4j.linalg.learning.config.Nesterovs
 import org.nd4j.linalg.lossfunctions.LossFunctions
+import org.nd4j.linalg.schedule.MapSchedule
+import org.nd4j.linalg.schedule.ScheduleType
 import java.io.File
 import java.util.*
 
 class ImagePixelAI(
     networkName: String,
-    val inputRadius: Int = 3,
+    private val inputWidth: Int,
+    private val inputHeight: Int,
+    private val outputWidth: Int,
+    private val outputHeight: Int,
+    private val offsetX: Int,
+    private val offsetY: Int,
     hiddenLayerCount: Int = 1,
     hiddenLayerSize: Int = 50,
-    val channels: List<Channel> = listOf(Channel.Red, Channel.Green, Channel.Blue),
+    private val useImageMirror: Boolean = true,
+    private val useImageRotation: Boolean = true,
+    private val useAdaptiveMinorityOversamplingFactor: Double = 1.0,
+    private val channels: List<Channel> = listOf(Channel.Red, Channel.Green, Channel.Blue),
     seed: Long = 123
 ) {
-    private val networkFile: File = File("${networkName}_radius${inputRadius}_layers${hiddenLayerCount}x${hiddenLayerSize}.zip")
+    private val networkFile: File = File("${networkName}_input${inputWidth}x${inputHeight}_layers${hiddenLayerCount}x${hiddenLayerSize}_output${outputWidth}x${outputHeight}_offset${offsetX}x${offsetY}.zip")
 
-    private var model: MultiLayerNetwork
-    private val inputImageDiameter = (inputRadius*2 + 1)
-    private val inputSize = inputImageDiameter * inputImageDiameter * channels.size
-    private val outputSize = channels.size
+    private val inputSize = inputWidth * inputHeight * channels.size
+    private val outputSize = outputWidth * outputHeight * channels.size
     private val random = Random(seed)
 
-    init {
-        if (networkFile.exists()) {
-            model = ModelSerializer.restoreMultiLayerNetwork(networkFile)
-        } else {
-            model = createNetwork(hiddenLayerCount, hiddenLayerSize)
-        }
+    private val model: MultiLayerNetwork = if (networkFile.exists()) {
+        ModelSerializer.restoreMultiLayerNetwork(networkFile)
+    } else {
+        createNetwork(hiddenLayerCount, hiddenLayerSize)
     }
 
     private fun createNetwork(hiddenLayerCount: Int, hiddenLayerSize: Int): MultiLayerNetwork {
         val seed = 123L;
 
+        val learningRateSchedule: MutableMap<Int, Double> = HashMap()
+        learningRateSchedule[0] = 0.001
+        learningRateSchedule[10] = 0.0001
+        val mapLearningRateSchedule = MapSchedule(ScheduleType.EPOCH, learningRateSchedule)
+
         val builder = NeuralNetConfiguration.Builder()
             .seed(seed)
             .l2(0.0005)
             .weightInit(WeightInit.XAVIER)
-            .updater(Adam(5e-4))
-            //.updater(Nesterovs(0.01, 0.9))
+            .updater(Adam(1e-3))
+            //.updater(Adam(0.0005))
+            //.updater(Adam(mapLearningRateSchedule))
+            //.updater(Nesterovs(0.001, 0.99))
             .list()
             .layer(
                 DenseLayer.Builder()
@@ -82,7 +93,7 @@ class ImagePixelAI(
                     .build()
             )
 
-        model = MultiLayerNetwork(builder.build())
+        val model = MultiLayerNetwork(builder.build())
         model.init()
 
         return model
@@ -95,8 +106,11 @@ class ImagePixelAI(
 
         for (i in 0 .. epochs) {
             for (inputFile in inputFiles) {
+                if (!inputFile.isFile) {
+                    continue
+                }
                 val outputFile = File(outputDirectory, inputFile.name)
-                if (!outputFile.exists()) {
+                if (!outputFile.exists() || !outputFile.isFile) {
                     continue
                 }
 
@@ -112,28 +126,120 @@ class ImagePixelAI(
     }
 
     fun train(input: Image, output: Image, epochs: Int = 10, pixelProbability: Double = 1.0) {
-        val dataSets = mutableListOf<DataSet>()
-        for (y in 0 until input.height) {
-            for (x in 0 until input.width) {
-                if (pixelProbability >= 1.0 || random.nextDouble() < pixelProbability) {
-                    val inputSub = input.crop(x-inputRadius, y-inputRadius, inputImageDiameter, inputImageDiameter)
-                    val outputSub = output.crop(x, y, 1, 1)
-                    val dataSet = toDataSet(inputSub, outputSub)
-                    dataSets.add(dataSet)
-                }
-            }
-        }
+        val dataSets = toDataSets(input, output, pixelProbability, useAdaptiveMinorityOversamplingFactor)
+        println("DataSet size ${dataSets.size}")
         dataSets.shuffle()
 
-        model.setListeners(
-            ScoreIterationListener(1))
-
         val dataSetIterator = ListDataSetIterator(dataSets)
+
         model.fit(dataSetIterator, epochs)
 
         println("Score: " + model.score())
 
         model.save(networkFile, true)
+    }
+
+    fun test(input: Image, output: Image) {
+        var transformedInput = input
+        var transformedOutput = output
+
+        if (useImageMirror) {
+            transformedInput = when(random.nextInt(3)) {
+                1 -> transformedInput.mirrorX()
+                2 -> transformedInput.mirrorY()
+                else -> transformedInput
+            }
+            transformedOutput = when(random.nextInt(3)) {
+                1 -> transformedOutput.mirrorX()
+                2 -> transformedOutput.mirrorY()
+                else -> transformedOutput
+            }
+        }
+
+        if (useImageRotation) {
+            transformedInput = when(random.nextInt(4)) {
+                1 -> transformedInput.rotateLeft()
+                2 -> transformedInput.rotateRight()
+                3 -> transformedInput.rotateRight().rotateRight()
+                else -> transformedInput
+            }
+            transformedOutput = when(random.nextInt(4)) {
+                1 -> transformedOutput.rotateLeft()
+                2 -> transformedOutput.rotateRight()
+                3 -> transformedOutput.rotateRight().rotateRight()
+                else -> transformedOutput
+            }
+        }
+
+        doTest(transformedInput, transformedOutput)
+    }
+
+    fun doTest(input: Image, output: Image) {
+        val dataSets = toDataSets(input, output)
+        val dataSetIterator = ListDataSetIterator(dataSets)
+
+        val evaluation: org.nd4j.evaluation.regression.RegressionEvaluation = model.evaluateRegression(dataSetIterator)
+        evaluation.columnNames = channels.map { c -> c.toString() }
+        println(evaluation.stats())
+    }
+
+    private fun toDataSets(input: Image, output: Image, pixelProbability: Double = 1.0, adaptiveMinorityOverSamplingFactor: Double = 1.0): MutableList<DataSet> {
+        val dataSets = mutableListOf<DataSet>()
+        if (adaptiveMinorityOverSamplingFactor < 1.0) {
+            val dataSetsWithDistance = mutableListOf<Pair<DataSet, Double>>()
+            for (y in 0 until input.height) {
+                for (x in 0 until input.width) {
+                    if (pixelProbability >= 1.0 || random.nextDouble() < pixelProbability) {
+                        val inputSub = input.crop(x-offsetX, y-offsetY, inputWidth, inputHeight)
+                        val outputSub = output.crop(x, y, outputWidth, outputHeight)
+                        val dataSet = toDataSet(inputSub, outputSub)
+
+                        val testOutput = model.output(dataSet.features, false)
+                        val dist = testOutput.distance2(dataSet.labels)
+                        dataSetsWithDistance.add(Pair(dataSet, dist))
+                    }
+                }
+            }
+            dataSetsWithDistance.sortBy { d -> -d.second }
+            val minoritySize = clamp((dataSetsWithDistance.size * adaptiveMinorityOverSamplingFactor + 0.5).toInt(), 1, dataSetsWithDistance.size)
+            for (i in 0 until minoritySize) {
+                dataSets.add(dataSetsWithDistance[i].first)
+            }
+        } else {
+            for (y in 0 until input.height) {
+                for (x in 0 until input.width) {
+                    if (pixelProbability >= 1.0 || random.nextDouble() < pixelProbability) {
+                        val inputSub = input.crop(x-offsetX, y-offsetY, inputWidth, inputHeight)
+                        val outputSub = output.crop(x, y, outputWidth, outputHeight)
+                        val dataSet = toDataSet(inputSub, outputSub)
+                        dataSets.add(dataSet)
+                    }
+                }
+            }
+        }
+        return dataSets
+
+
+//        val dataSets = mutableListOf<DataSet>()
+//        for (y in 0 until input.height) {
+//            for (x in 0 until input.width) {
+//                if (pixelProbability >= 1.0 || random.nextDouble() < pixelProbability) {
+//                    val inputSub = input.crop(x-offsetX, y-offsetY, inputWidth, inputHeight)
+//                    val outputSub = output.crop(x, y, outputWidth, outputHeight)
+//                    val dataSet = toDataSet(inputSub, outputSub)
+//                    if (adaptiveMinorityOverSampling) {
+//                        val testOutput = model.output(dataSet.features, false)
+//                        val dist = testOutput.distance2(dataSet.labels)
+//                        if (random.nextDouble() < dist) {
+//                            dataSets.add(dataSet)
+//                        }
+//                    } else {
+//                        dataSets.add(dataSet)
+//                    }
+//                }
+//            }
+//        }
+//        return dataSets
     }
 
     private fun toDataSet(input: Image, output: Image): DataSet {
@@ -143,7 +249,9 @@ class ImagePixelAI(
             input[channels[channelIndex]][pixelIndex]
         }
         val outputArray = DoubleArray(outputSize) { i ->
-            output[channels[i]][0]
+            val pixelIndex = i / channels.size
+            val channelIndex = i % channels.size
+            output[channels[channelIndex]][pixelIndex]
         }
 
         //val inputShape = intArrayOf(inputSize)
@@ -158,9 +266,9 @@ class ImagePixelAI(
         val inputShape = intArrayOf(inputSize)
 
         val output = MatrixImage(input.width, input.height, channels)
-        for (y in 0 until output.height) {
-            for (x in 0 until output.width) {
-                val inputSub = input.crop(x-inputRadius, y-inputRadius, inputImageDiameter, inputImageDiameter)
+        for (y in 0 until output.height step outputHeight) {
+            for (x in 0 until output.width step outputWidth) {
+                val inputSub = input.crop(x-offsetX, y-offsetY, inputWidth, inputHeight)
                 val inputArray = DoubleArray(inputSize) { i ->
                     val pixelIndex = i / channels.size
                     val channelIndex = i % channels.size
@@ -169,8 +277,13 @@ class ImagePixelAI(
                 val inputNdArray: INDArray = Nd4j.create(inputArray)
                 val outputNdArray = model.output(inputNdArray)
 
-                for (i in channels.indices) {
-                    output[channels[i]][x, y] = outputNdArray.getDouble(i)
+                for (yOutput in 0 until outputHeight) {
+                    for (xOutput in 0 until outputWidth) {
+                        for (channelIndex in channels.indices) {
+                            val outputIndex = (xOutput + yOutput * outputWidth) * channels.size + channelIndex
+                            output[channels[channelIndex]][x+xOutput, y+yOutput] = clamp(outputNdArray.getDouble(outputIndex), 0.0, 1.0)
+                        }
+                    }
                 }
             }
         }
--- src/test/kotlin/TestAI.kt
+++ src/test/kotlin/TestAI.kt
@@ -1,19 +1,96 @@
 import ch.obermuhlner.kimage.ai.ImagePixelAI
+import ch.obermuhlner.kimage.image.crop
 import ch.obermuhlner.kimage.io.ImageReader
 import ch.obermuhlner.kimage.io.ImageWriter
 import ch.obermuhlner.kimage.sobelFilter3
+import org.nd4j.evaluation.regression.RegressionEvaluation
 import java.io.File
 
 object TestAI {
     @JvmStatic
     fun main(args: Array<String>) {
-        val radius = 2
-        val hiddenLayerCount = 0
+        testSobel()
+        //testDebayer()
+    }
+
+    fun testDebayer() {
+        val inputWidth = 6
+        val inputHeight = 6
+        val outputWidth = 2
+        val outputHeight = 2
+        val offsetX = 2
+        val offsetY = 2
+        val hiddenLayerCount = 1
         val hiddenLayerSize = 10
         val pixelProbability = 1.0
 
-        val description = "radius${radius}_layers${hiddenLayerCount}x${hiddenLayerSize}"
-        val ai = ImagePixelAI("gauss", radius, hiddenLayerCount, hiddenLayerSize)
+        val description = "input${inputWidth}x${inputHeight}_layers${hiddenLayerCount}x${hiddenLayerSize}_output${outputWidth}x${outputHeight}_offset${offsetX}x${offsetY}"
+
+        val ai = ImagePixelAI(
+            "debayer",
+            inputWidth,
+            inputHeight,
+            outputWidth,
+            outputHeight,
+            offsetX,
+            offsetY,
+            hiddenLayerCount,
+            hiddenLayerSize)
+
+        val trainInput = ImageReader.read(File("images/misc/bayer/bayer_from_debayer_superpixel/IMG_2426.tif"))
+        val trainOutput = ImageReader.read(File("images/misc/bayer/debayer_superpixel/IMG_2426.tif"))
+
+        val testInput = ImageReader.read(File("images/misc/bayer/orig_raw/convert-raw_IMG_2425.tif")).crop(2800, 2000, 1000, 1000)
+
+        var epoch = 0
+        for (i in 0 until 1000) {
+            for (j in 0 until 1) {
+                ai.train(trainInput, trainOutput, epochs = 1, pixelProbability = pixelProbability)
+                epoch++
+            }
+
+            println("Write output images after $epoch epochs")
+            val testOutput = ai.run(testInput)
+            ImageWriter.write(testOutput, File("ai_output_${description}_epoch${epoch}.png"))
+
+            val testTrainOutput = ai.run(trainInput)
+            ImageWriter.write(testTrainOutput, File("ai_train_output_${description}_epoch${epoch}.png"))
+        }
+
+//        ai.train(
+//            File("images/misc/bayer/debayer_superpixel"),
+//            File("images/misc/bayer/bayer_from_debayer_superpixel"),
+//            listOf("tif"),
+//            1,
+//            1,
+//        1.0)
+//
+//        val testOutput = ai.run(testInput)
+//        ImageWriter.write(testOutput, File("test_output_$description.tif"))
+    }
+
+    fun testSobel() {
+        val inputWidth = 3
+        val inputHeight = 3
+        val outputWidth = 1
+        val outputHeight = 1
+        val offsetX = 1
+        val offsetY = 1
+        val hiddenLayerCount = 0
+        val hiddenLayerSize = 50
+        val pixelProbability = 1.0
+
+        val description = "input${inputWidth}x${inputHeight}_layers${hiddenLayerCount}x${hiddenLayerSize}_output${outputWidth}x${outputHeight}_offset${offsetX}x${offsetY}"
+        val ai = ImagePixelAI(
+            "sobel3",
+            inputWidth,
+            inputHeight,
+            outputWidth,
+            outputHeight,
+            offsetX,
+            offsetY,
+            hiddenLayerCount,
+            hiddenLayerSize)
 
         val trainInput = ImageReader.read(File("images/lena512.png"))
         val trainOutput = trainInput.sobelFilter3()
@@ -30,6 +107,8 @@ object TestAI {
                 ai.train(trainInput, trainOutput, epochs = 1, pixelProbability = pixelProbability)
                 epoch++
             }
+
+            ai.test(testInput, testRefOutput)
 
             println("Write output images after $epoch epochs")
             val testOutput = ai.run(testInput)
