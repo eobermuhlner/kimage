3f9717da6abb8660de1b3af04033e0fb305ad8c0
Eric Obermühlner <eobermuhlner@gmail.com>
2023-04-29 13:42:20 +0200

    rename-file script to renumber by appending index

--- src/main/dist/.kimage/rename-file.kts
+++ src/main/dist/.kimage/rename-file.kts
@@ -14,27 +14,35 @@ kimage(0.1) {
     name = "rename-file"
     title = "Copy and rename files into another directory"
     description = """
-                Rename images.
+                Rename and renumber images.
                 """
     arguments {
-        file("target") {
+        string("name") {
+            description = """
+                The name of the renamed file.
+                The index of the renamed file will be appended at the end of the filename.
+                """
+        }
+        int("start") {
             description = """
                 The target directory to move the image file into.
                 """
-            isDirectory = true
+            default = 1
         }
     }
 
     multi {
-        val target: File by arguments
+        val name: String by arguments
+        val start: Int by arguments
 
-        if (!target.exists()) {
-            target.mkdirs()
-        }
-
+        var index = start
         for (inputFile in inputFiles) {
-            println("Moving $inputFile into $target directory")
-            Files.move(inputFile.toPath(), File(target, inputFile.name).toPath())
+            val outputFileName = name + "%04d".format(index) + "." + inputFile.extension
+            println("Renaming $inputFile into $outputFileName")
+            val outputFile = File(inputFile.parentFile, outputFileName)
+            Files.copy(inputFile.toPath(), outputFile.toPath())
+
+            index++
         }
     }
 }
