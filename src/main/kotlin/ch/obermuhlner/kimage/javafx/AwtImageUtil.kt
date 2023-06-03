604d9d4e0e5810453f83e9c95cf743cd13142587
Eric Obermühlner <eobermuhlner@gmail.com>
2023-06-03 12:13:22 +0200

    fix margins in graphics()

--- src/main/kotlin/ch/obermuhlner/kimage/javafx/AwtImageUtil.kt
+++ src/main/kotlin/ch/obermuhlner/kimage/javafx/AwtImageUtil.kt
@@ -44,7 +44,7 @@ object AwtImageUtil {
         return result
     }
 
-    fun graphics(image: Image, marginTop: Int, marginLeft: Int, marginBottom: Int, marginRight: Int, func: (java.awt.Graphics2D, Int, Int) -> Unit): Image {
+    fun graphics(image: Image, marginTop: Int, marginLeft: Int, marginBottom: Int, marginRight: Int, func: (java.awt.Graphics2D, Int, Int, Int, Int) -> Unit): Image {
         val width = image.width + marginLeft + marginRight
         val height = image.height + marginTop + marginBottom
         val result = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
@@ -56,9 +56,9 @@ object AwtImageUtil {
         graphics.clearRect(0, 0, width, height)
 
         val bufferedImage = toBufferedImage(image)
-        graphics.drawImage(bufferedImage, marginTop, marginLeft, null)
+        graphics.drawImage(bufferedImage, marginLeft, marginTop, null)
 
-        func(graphics, width, height)
+        func(graphics, width, height, marginLeft, marginTop)
 
         graphics.dispose()
         return toImage(result)
