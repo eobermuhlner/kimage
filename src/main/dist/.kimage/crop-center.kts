43b42cd263865a8740fe32bfebce1904e24625a9
Eric Obermühlner <eobermuhlner@gmail.com>
2023-09-07 06:31:36 +0200

    add avoidBorders parameter

--- src/main/dist/.kimage/crop-center.kts
+++ src/main/dist/.kimage/crop-center.kts
@@ -42,16 +42,21 @@ kimage(0.1) {
                 The radius along the y axis to crop.
                 """
         }
+        boolean("avoidBorders") {
+            description = """
+                Avoid clipping borders by moving the cropped area.
+                """
+            default = false
+        }
     }
 
     single {
         var x: Optional<Int> by arguments
         var y: Optional<Int> by arguments
-        var x2: Optional<Int> by arguments
-        var y2: Optional<Int> by arguments
         var radius: Optional<Int> by arguments
         var radiusX: Optional<Int> by arguments
         var radiusY: Optional<Int> by arguments
+        var avoidBorders: Boolean by arguments
 
         if (!x.isPresent) {
             x = Optional.of(inputImage.width / 2)
@@ -60,30 +65,47 @@ kimage(0.1) {
             y = Optional.of(inputImage.height / 2)
         }
 
+        if (!radiusX.isPresent) {
+            radiusX = Optional.of(inputImage.width / 4)
+        }
+        if (!radiusY.isPresent) {
+            radiusY = Optional.of(inputImage.height / 4)
+        }
+
+        var xx = x.get()
+        var yy = y.get()
+        if (avoidBorders) {
+            if (radius.isPresent) {
+                xx = if (xx - radius.get() < 0) radius.get() else xx
+                yy = if (yy - radius.get() < 0) radius.get() else yy
+                xx = if (xx + radius.get() > inputImage.width) inputImage.width - radius.get() else xx
+                yy = if (yy + radius.get() > inputImage.height) inputImage.height - radius.get() else yy
+            } else {
+                xx = if (xx - radiusX.get() < 0) radiusX.get() else xx
+                yy = if (yy - radiusY.get() < 0) radiusY.get() else yy
+                xx = if (xx + radiusX.get() > inputImage.width) inputImage.width - radiusX.get() else xx
+                yy = if (yy + radiusY.get() > inputImage.height) inputImage.height - radiusY.get() else yy
+            }
+        }
+
         if (radius.isPresent) {
             println("Arguments:")
-            println("  x = ${x.get()}")
-            println("  y = ${y.get()}")
+            println("  x = $xx")
+            println("  y = $yy")
             println("  radius = ${radius.get()}")
             println()
 
-            inputImage.cropCenter(radius.get(), x.get(), y.get())
+            inputImage.cropCenter(radius.get(), xx, yy)
         } else {
-            if (!radiusX.isPresent) {
-                radiusX = Optional.of(inputImage.width / 4)
-            }
-            if (!radiusY.isPresent) {
-                radiusY = Optional.of(inputImage.height / 4)
-            }
 
             println("Arguments:")
-            println("  x = ${x.get()}")
-            println("  y = ${y.get()}")
+            println("  x = $xx")
+            println("  y = $yy")
             println("  radiusX = ${radiusX.get()}")
             println("  radiusY = ${radiusY.get()}")
             println()
 
-            inputImage.cropCenter(radiusX.get(), radiusY.get(), x.get(), y.get())
+            inputImage.cropCenter(radiusX.get(), radiusY.get(), xx, yy)
         }
     }
 }
